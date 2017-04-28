05d15ffb78cd9f0d96434cf33fe5207a6d73dfbf
xycyx <yxcheng@uw.edu>
2017-04-28 15:03:20 -0700

    SentTheMail

---
+++ sentTheEmail.m
@@ -0,0 +1,32 @@
+function [ ] = sentTheEmail( yourMail, password, toMail, host, port)
+%sentTheEmail sent a email to a address
+%   mail: your email address 'XXXX@uw.edu'
+%   password: your password 'XXXXXX'
+%   toMail, to this mail 'XXXX@gmail.com'
+%   host: SMTP host server    'smtp.uw.edu'
+%   port: SMTP port   '993'
+
+%uw emial default set
+if (~exist('host', 'var'))
+    host = 'smtp.uw.edu';
+end
+if (~exist('port', 'var'))
+    port = '993';
+end
+    
+setpref('Internet','SMTP_Server', host);
+setpref('Internet','E_mail',yourMail);
+setpref('Internet','SMTP_Username',yourMail);
+setpref('Internet','SMTP_Password',password);
+
+props = java.lang.System.getProperties;
+props.setProperty('mail.smtp.auth','true');
+props.setProperty('mail.smtp.socketFactory.class', 'javax.net.ssl.SSLSocketFactory');
+props.setProperty('mail.smtp.socketFactory.port',port);
+% Send the email
+
+sendmail(toMail,'MATLAB Progrem Status','Hello! Your program has been completed!');
+
+
+end
+
