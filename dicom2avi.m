f85528498bbbd6e28f9602308e20cc9ea5ce7c56
Yuxuan Cheng <xycyx@users.noreply.github.com>
2018-06-21 13:00:58 -0700

    Update dicom2avi uint16 support

--- dicom2avi.m
+++ dicom2avi.m
@@ -1,10 +1,14 @@
 function [] = dicom2avi(file_name,out_file_name)
-%transfer 0-255 dicom to 0-255 avi
+%transfer uint8 or uint16 dicom to 0-255 avi
 %   Detailed explanation goes here
 % file_name = 'registered2.dcm';
 
 img = dicomread(file_name);
 
+% if it was uint16
+if isa(img, 'uint16')
+    img = img./256;
+end
 [depth,width, trans, frame_num] = size(img);
 
 if nargin < 2
